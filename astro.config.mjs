acfe0ef632ffa679efa3c65f9ce88ca5524b05b3
Urvashi0109 <91434560+Urvashi0109@users.noreply.github.com>
2026-02-17 20:35:55 +0530

    add explicit base to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://urvashi0109.github.io',
-  base: '/'
+  base: '/',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
   markdown: {
     extendDefaultPlugins: true,
